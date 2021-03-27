9b7b25b4ddd69bc421e89c56297c00e8ccbc70a1
TimothyKodz <55055311+TimothyKodz@users.noreply.github.com>
2021-03-27 14:35:09 -0400

    Updates to DatabaseInterface.java to run code from main method

--- MediForm/src/main/java/DatabaseInterface.java
+++ MediForm/src/main/java/DatabaseInterface.java
@@ -7,21 +7,24 @@ import org.bson.Document;
 import javax.print.Doc;
 
 public class DatabaseInterface {
-    ConnectionString connString = new ConnectionString(
+
+    public static void main(String[] args) throws UnknownHostException{
+        ConnectionString connString = new ConnectionString(
         "mongodb+srv://test-user-01:*****@mediform-cluster-1.t6zjg.mongodb.net/test?authSource=admin&replicaSet=atlas-2ds9so-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
-    );
-    MongoClientSettings settings = MongoClientSettings.builder()
-        .applyConnectionString(connString)
-        .retryWrites(true)
-        .build();
-    MongoClient mongoClient = MongoClients.create(settings);
+        );
+        MongoClientSettings settings = MongoClientSettings.builder()
+            .applyConnectionString(connString)
+            .retryWrites(true)
+            .build();
+        MongoClient mongoClient = MongoClients.create(settings);
 
-    MongoDatabase database = mongoClient.getDatabase("MediForm");
-    MongoCollection<Document> dbCollection = database.getCollection("user");
+        MongoDatabase database = mongoClient.getDatabase("MediForm");
+        MongoCollection<Document> dbCollection = database.getCollection("user");
 
-    Document document = new Document("username", "test")
-        .append("password", "testPass")
-        .append("role", "nurse");
+        Document document = new Document("username", "test")
+            .append("password", "testPass")
+            .append("role", "nurse");
 
-    dbCollection.insertOne(document);
-}   
+        dbCollection.insertOne(document);
+    }   
+}
