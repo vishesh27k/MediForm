35802206207d1958d13dacb3a665d0bab4763a78
TimothyKodz <55055311+TimothyKodz@users.noreply.github.com>
2021-03-27 16:26:51 -0400

    Move database variables outside of main method to be accessed by all methods in class

--- MediForm/src/main/java/DatabaseInterface.java
+++ MediForm/src/main/java/DatabaseInterface.java
@@ -1,30 +1,30 @@
 import com.mongodb.*;
 import com.mongodb.client.*;
 import java.net.UnknownHostException;
-
 import org.bson.Document;
-
 import javax.print.Doc;
 
 public class DatabaseInterface {
 
-    public static void main(String[] args) throws UnknownHostException{
-        ConnectionString connString = new ConnectionString(
-        "mongodb+srv://test-user-01:*****@mediform-cluster-1.t6zjg.mongodb.net/test?authSource=admin&replicaSet=atlas-2ds9so-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
+    public static ConnectionString connString = new ConnectionString(
+        "mongodb+srv://test-user-01:testPass01@mediform-cluster-1.t6zjg.mongodb.net/test?authSource=admin&replicaSet=atlas-2ds9so-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
         );
-        MongoClientSettings settings = MongoClientSettings.builder()
-            .applyConnectionString(connString)
-            .retryWrites(true)
-            .build();
-        MongoClient mongoClient = MongoClients.create(settings);
+    public static MongoClientSettings settings = MongoClientSettings.builder()
+        .applyConnectionString(connString)
+        .retryWrites(true)
+        .build();
+    public static MongoClient mongoClient = MongoClients.create(settings);
 
-        MongoDatabase database = mongoClient.getDatabase("MediForm");
-        MongoCollection<Document> dbCollection = database.getCollection("user");
+    public static MongoDatabase database = mongoClient.getDatabase("MediForm");
+    public static MongoCollection<Document> collectionUser = database.getCollection("user");
+    public static MongoCollection<Document> collectionPatient = database.getCollection("patient");
+
+    public static void main(String[] args) throws UnknownHostException{
 
         Document document = new Document("username", "test")
             .append("password", "testPass")
             .append("role", "nurse");
 
-        dbCollection.insertOne(document);
+        collectionUser.insertOne(document);
     }   
 }
