7f1a86a495ba53c81591284dea079878f4a535b8
TimothyKodz <55055311+TimothyKodz@users.noreply.github.com>
2021-04-09 19:33:29 -0400

    Initial framework for controllers

--- MediForm/src/main/java/notesController.java
+++ MediForm/src/main/java/notesController.java
@@ -1,3 +1,22 @@
-public class notesController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class notesController implements Initializable{
     
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
