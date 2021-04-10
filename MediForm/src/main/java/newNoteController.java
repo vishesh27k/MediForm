49692c42ee21915de140f98b2b74d61c3309835b
TimothyKodz <55055311+TimothyKodz@users.noreply.github.com>
2021-04-09 21:59:04 -0400

    Split notes into view and new

--- MediForm/src/main/java/newNoteController.java
+++ MediForm/src/main/java/newNoteController.java
@@ -13,8 +13,17 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class notesController implements Initializable{
-    
+public class newNoteController implements Initializable{
+    @FXML
+    void saveNote(ActionEvent event) throws IOException {
+        Parent nurseMenuParent = FXMLLoader.load(getClass().getResource("nurseMenu.fxml"));
+        Scene nurseMenuScene = new Scene(nurseMenuParent);
+        
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(nurseMenuScene);
+        window.show();
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
