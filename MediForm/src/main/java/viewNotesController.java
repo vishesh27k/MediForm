d13db2cc2c96497237b83a99bcaaac1fc6ad724c
pfundaro9 <78462157+pfundaro9@users.noreply.github.com>
2021-04-12 17:36:26 -0400

    Notes Updated

--- MediForm/src/main/java/viewNotesController.java
+++ MediForm/src/main/java/viewNotesController.java
@@ -6,6 +6,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -14,11 +15,26 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class viewNotesController implements Initializable{
+
+
+    @FXML
+    private TextArea notes1;
+
     @FXML
     void toNewNote(ActionEvent event) throws IOException {
         Parent newNoteParent = FXMLLoader.load(getClass().getResource("newNote.fxml"));
         Scene newNoteScene = new Scene(newNoteParent);
         
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(newNoteScene);
+        window.show();
+    }
+
+    @FXML
+    void toMainMenu1(ActionEvent event) throws IOException {
+        Parent newNoteParent = FXMLLoader.load(getClass().getResource("physicianMenu.fxml"));
+        Scene newNoteScene = new Scene(newNoteParent);
+
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
         window.setScene(newNoteScene);
         window.show();
