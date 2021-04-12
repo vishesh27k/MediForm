681d747f7da0e523278039624cd8d7c9cfd8b24a
pfundaro9 <78462157+pfundaro9@users.noreply.github.com>
2021-04-12 16:22:03 -0400

    Updated Diagnosis

--- MediForm/src/main/java/diagnoseController.java
+++ MediForm/src/main/java/diagnoseController.java
@@ -1,2 +1,43 @@
-public class diagnoseController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class diagnoseController implements Initializable {
+    @FXML
+    private CheckBox brokenBone;
+
+    @FXML
+    private CheckBox heartAttack;
+
+    @FXML
+    private CheckBox laceration;
+
+    @FXML
+    private CheckBox hivAids;
+
+    @FXML
+    private CheckBox liverFailure;
+
+    @FXML
+    private CheckBox kidneyFailure;
+
+    @FXML
+    private CheckBox diabetes;
+
+    @FXML
+    private CheckBox inflammatoryBowlDisease;
+
+    @FXML
+    private CheckBox stroke;
+
+    @FXML
+    private CheckBox tornMuscleTendon;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
