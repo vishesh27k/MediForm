c6a41d6a49f0938f33bcdf0c9f5a9dcbee4e1388
pfundaro9 <78462157+pfundaro9@users.noreply.github.com>
2021-04-12 16:37:39 -0400

    Order Test Updated

--- MediForm/src/main/java/orderTestController.java
+++ MediForm/src/main/java/orderTestController.java
@@ -1,2 +1,44 @@
-public class orderTestController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class orderTestController implements Initializable {
+
+    @FXML
+    private CheckBox nucleicAcid;
+
+    @FXML
+    private CheckBox coagulationPanel;
+
+    @FXML
+    private CheckBox dheaSulfateSerum;
+
+    @FXML
+    private CheckBox cReactiveProtein;
+
+    @FXML
+    private CheckBox alc;
+
+    @FXML
+    private CheckBox xRay;
+
+    @FXML
+    private CheckBox mri;
+
+    @FXML
+    private CheckBox ctScan;
+
+    @FXML
+    private CheckBox urinalysis;
+
+    @FXML
+    private CheckBox stoolCulture;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
