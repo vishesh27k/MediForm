6af46943061561e1ae85da6b4e3775378c81eca6
pfundaro9 <78462157+pfundaro9@users.noreply.github.com>
2021-04-16 18:35:42 -0400

    Added viewRegForm

--- MediForm/src/main/java/viewRegFormController.java
+++ MediForm/src/main/java/viewRegFormController.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,57 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class viewRegFormController implements Initializable{
+
+    @FXML
+    private TextField firstName1;
+
+    @FXML
+    private TextField lastName1;
+
+    @FXML
+    private TextField dateOfBirth1;
+
+    @FXML
+    private TextField address1;
+
+    @FXML
+    private TextField city1;
+
+    @FXML
+    private TextField state1;
+
+    @FXML
+    private TextField phoneNumber1;
+
+    @FXML
+    private TextField insuranceProvider1;
+
+    @FXML
+    private TextField primaryPhysician1;
+
+    @FXML
+    private TextField emergContactName1;
+
+    @FXML
+    private TextField emergPhoneNumber1;
+
+    @FXML
+    private TextField reasonForVisit1;
+
+    @FXML
+    private TextField insuranceID1;
+
+    @FXML
+    private TextField currentMeds1;
+
+    @FXML
+    private TextField medHistory1;
+
+    @FXML
+    private TextField dateOfVisit1;
+
+    @FXML
+    private TextField patientID1;
     
     @FXML
     void toRegHome(ActionEvent event) throws IOException {
