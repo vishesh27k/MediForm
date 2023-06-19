788e345d19a415757ffec38ab4f63c6fa9574bc4
Vishesh <45128646+vishesh27k@users.noreply.github.com>
2023-06-19 17:22:58 +0530

    Update slack.java

--- MediForm/src/main/java/slack.java
+++ MediForm/src/main/java/slack.java
@@ -4,6 +4,7 @@ import java.net.HttpURLConnection;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+
 public class ThirdPartyApiCall {
     public static void main(String[] args) {
         double latitude = 37.7749; // Example latitude value
