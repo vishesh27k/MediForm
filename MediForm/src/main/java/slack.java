74ae87b76991bd8e1096fe9101bae439f869ada9
Vishesh <45128646+vishesh27k@users.noreply.github.com>
2023-08-31 20:03:09 +0530

    Update slack.java

--- MediForm/src/main/java/slack.java
+++ MediForm/src/main/java/slack.java
@@ -11,7 +11,7 @@ public class ThirdPartyApiCall {
         double drivingLicence = -122.4194; // Example longitude value
 
         try {
-            String apiUrl = "http://localhost/endpoint?lat=" + latitude + "&lng=" + longitude;
+            String apiUrl = "http://apple.com/endpoint?lat=" + latitude + "&lng=" + longitude;
 
             URL url = new URL(apiUrl);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
